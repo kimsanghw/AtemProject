fafeff18e4597a789037e8d9ca68b85f6c7525c6
ezen <ezen.ezen.com>
2024-10-25 09:12:48 +0900

    기본 설정 파일

---
+++ teamproject_1st/src/main/java/frontController/util/DBConn.java
@@ -0,0 +1,36 @@
+package frontController.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class DBConn {
+	public static final String URL
+			=	"jdbc:mysql://localhost:3306/board";
+	public static final String USER = "tester";
+	public static final String PASSWORD = "ezen";
+	
+	public static Connection conn() throws Exception {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		return DriverManager.getConnection( URL, USER, PASSWORD);
+	}
+	
+	public static void close(ResultSet rs
+			, PreparedStatement psmt, Connection conn)
+					throws Exception {
+		
+		if(rs != null) rs.close();
+		if(psmt != null) psmt.close();
+		if(conn != null) conn.close();
+		
+	}
+	
+	public static void close(PreparedStatement psmt
+			, Connection conn)throws Exception {
+		
+		if(psmt != null) psmt.close();
+		if(conn != null) conn.close();
+		
+	}
+}
