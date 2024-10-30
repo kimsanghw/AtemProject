e6b9493efddc51617bc4d233aaa5b71564e2ecfa
MYCOM <ezen@ezen.com>
2024-10-30 13:57:26 +0900

    1357

--- teamproject_1st/src/main/java/FrontController/UserController.java
+++ teamproject_1st/src/main/java/FrontController/UserController.java
@@ -28,12 +28,12 @@ public class UserController {
 				 join(request, response);
 		    } else if(request.getMethod().equals("POST")){
 		        joinOk(request, response);
-		    } else if(comments[comments.length-1].equals("logout.do")) {
-		    	if(request.getMethod().equals("GET")) {
-		    		logout(request,response);
-		    	}
 		    }
-		}else if(comments[comments.length-1].equals("checkEmail.do")){ 
+		}else if(comments[comments.length-1].equals("logout.do")) {
+	    	if(request.getMethod().equals("GET")) {
+	    		logout(request,response);
+	    	}
+	    }else if(comments[comments.length-1].equals("checkEmail.do")){ 
 			if(request.getMethod().equals("GET")){
 			checkEmail(request, response);
 			} else if(request.getMethod().equals("POST")){
