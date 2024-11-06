85c36b87b964f9388a26d94465dc3fa544f2a7a7
MYCOM <ezen@ezen.com>
2024-11-06 11:25:16 +0900

    AttendanceController mypage mypage2

--- teamproject_1st/src/main/java/FrontController/AttendanceController.java
+++ teamproject_1st/src/main/java/FrontController/AttendanceController.java
@@ -165,7 +165,7 @@ public class AttendanceController {
 			
 			rs = psmt.executeQuery();
 			
-			 while(rs.next()) {
+			 if(rs.next()) {
 				 
 					ClassVO vo = new ClassVO();
 					vo.setCno(rs.getInt("cno"));
@@ -175,7 +175,8 @@ public class AttendanceController {
 					vo.setSubject(rs.getString("subject"));
 					vo.setDuringclass(rs.getString("duringclass"));
 					vo.setEnd_duringclass(rs.getString("End_duringclass"));
-					vo.setDifficult(rs.getString("difficult"));
+					vo.setDifficult(rs.getString(""
+							+ "difficult"));
 					vo.setBook(rs.getString("book"));
 					
 					
